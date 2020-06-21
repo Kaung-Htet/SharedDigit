4e755630a4e05389fd63c9fdd959da5da35fd4a1
Kaung-Htet <kaunghtetnyilay@gmail.com>
2020-06-21 22:39:58 +0700

    Add hasSharedDigit method

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,14 @@
 public class Main {
+    public static void main(String[] args) {
+        System.out.println(SharedDigit.hasSharedDigit(12,23));
+        System.out.println(SharedDigit.hasSharedDigit(9,99));
+        System.out.println(SharedDigit.hasSharedDigit(15,55));
+        System.out.println(SharedDigit.hasSharedDigit(12, 91));
+    }
 }
+
+//* hasSharedDigit(12, 23); → should return true since the digit 2 appears in both numbers
+//
+//* hasSharedDigit(9, 99); → should return false since 9 is not within the range of 10-99
+//
+//* hasSharedDigit(15, 55); → should return true since the digit 5 appears in both numbers
--- src/SharedDigit.java
+++ src/SharedDigit.java
@@ -1,11 +1,42 @@
 public class SharedDigit {
+    public static boolean hasSharedDigit (int num1, int num2){
+        if (num1 < 10 || num1 > 99 || num2 < 10 || num2 > 99){
+            return false;
+        }
+
+        while (num1 > 0){
+            int remainder1 = num1 % 10;
+
+            //reset num2 value to original value after while loop of num2
+            int loopNum2 = num2 ;
+
+            while(loopNum2 > 0){
+                int remainder2 = loopNum2 % 10;
+
+                //check shared number
+                if (remainder1 == remainder2){
+                    return true;
+                }
+
+                //for num2 looping
+                loopNum2 /= 10;
+            }
+
+            //for num1 looping
+            num1 /= 10;
+        }
+
+        return false;
+    }
 }
 
 //Write a method named hasSharedDigit with two parameters of type int.
 //
-//Each number should be within the range of 10 (inclusive) - 99 (inclusive). If one of the numbers is not within the range, the method should return false.
+//Each number should be within the range of 10 (inclusive) - 99 (inclusive).
+// If one of the numbers is not within the range, the method should return false.
 //
-//The method should return true if there is a digit that appears in both numbers, such as 2 in 12 and 23; otherwise, the method should return false.
+//The method should return true if there is a digit that appears in both numbers, such as 2 in 12 and 23;
+// otherwise, the method should return false.
 //
 //
 //EXAMPLE INPUT/OUTPUT:
